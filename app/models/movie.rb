4f54b188169ecae83ac86d898038d49741bb1896
HTGAzureX1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2026-07-13 16:28:18 -0700

    part 1: movie filtering

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ApplicationRecord
+  def self.all_ratings
+    Movie.distinct.pluck(:rating)
+  end
+
+  def self.with_ratings(ratings)
+    if ratings.nil? || ratings.empty?
+      Movie.all
+    else
+      Movie.where(rating: ratings)
+    end
+  end
 end
